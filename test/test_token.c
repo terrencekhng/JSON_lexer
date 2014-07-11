ef0bc4d2ae5bc2a42636a858720d0eab833e2b0a
Terence Ng <awesome.terence@gmail.com>
2014-07-11 16:19:30 +0800

    create test directory

---
+++ test/test_token.c
@@ -0,0 +1,57 @@
+/* test_token.c */
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+#include"token.h"
+#include"lexer.h"
+#include"utils.h"
+
+static int tokens = 0;
+
+const char **parse_args(int *argc, const char **argv) {
+	const char *arg, **args = argv;
+	
+	int i, len;
+	for (i = 0, len = *argc; i < len; ++i) {
+		arg = args[i];
+		
+		if (!strcmp("-T", arg) || !strcmp("--tokens", arg)) {
+			tokens = 1;
+			--*argc;
+			++argv;
+		} else if ('-' == arg[0]) {
+			fprintf(stderr, "unknown flag %s\n", arg);
+			exit(1);
+		}
+	}
+}
+
+int eval(char *source, const char *path) {
+	json_lexer_t lex;
+	json_lexer_init(&lex, source, path);
+
+	if (tokens) {
+		while (json_scan(&lex)) {
+			printf(" %d ", lex.lineno);
+			json_token_inspect(&lex.tok);
+		}
+		return 0;
+	}
+}
+
+int main(int argc, const char **argv)
+{
+	char *source;
+
+	// parse arguments
+	argv = parse_args(&argc, argv);
+
+	// eval stdin
+	if (1 == argc) {
+		source = read_until_eof(stdin);
+		return eval(source, "stdin");
+	}
+}

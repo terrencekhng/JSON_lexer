e27160a2b8114487e69233e1ac6ef0fab9f37b3f
Terence Ng <pheotiman@gmail.com>
2014-07-12 22:21:51 +0800

    indented

--- test/test_token.c
+++ test/test_token.c
@@ -5,53 +5,55 @@
 #include<stdlib.h>
 
 
-#include"token.h"
-#include"lexer.h"
-#include"utils.h"
+#include"../src/token.h"
+#include"../src/lexer.h"
+#include"../src/utils.h"
 
 static int tokens = 0;
 
 const char **parse_args(int *argc, const char **argv) {
-	const char *arg, **args = argv;
-	
-	int i, len;
-	for (i = 0, len = *argc; i < len; ++i) {
-		arg = args[i];
-		
-		if (!strcmp("-T", arg) || !strcmp("--tokens", arg)) {
-			tokens = 1;
-			--*argc;
-			++argv;
-		} else if ('-' == arg[0]) {
-			fprintf(stderr, "unknown flag %s\n", arg);
-			exit(1);
-		}
-	}
+  const char *arg, **args = argv;
+
+  int i, len;
+  for (i = 0, len = *argc; i < len; ++i) {
+    arg = args[i];
+
+    if (!strcmp("-T", arg) || !strcmp("--tokens", arg)) {
+      tokens = 1;
+      --*argc;
+      ++argv;
+    } else if ('-' == arg[0]) {
+      fprintf(stderr, "unknown flag %s\n", arg);
+      exit(1);
+    }
+  }
+
+  return argv;
 }
 
 int eval(char *source, const char *path) {
-	json_lexer_t lex;
-	json_lexer_init(&lex, source, path);
+  json_lexer_t lex;
+  json_lexer_init(&lex, source, path);
 
-	if (tokens) {
-		while (json_scan(&lex)) {
-			printf(" %d ", lex.lineno);
-			json_token_inspect(&lex.tok);
-		}
-		return 0;
-	}
+  if (tokens) {
+    while (json_scan(&lex)) {
+      printf(" %d ", lex.lineno);
+      json_token_inspect(&lex.tok);
+    }
+    return 0;
+  }
 }
 
 int main(int argc, const char **argv)
 {
-	char *source;
+  char *source;
 
-	// parse arguments
-	argv = parse_args(&argc, argv);
+  // parse arguments
+  argv = parse_args(&argc, argv);
 
-	// eval stdin
-	if (1 == argc) {
-		source = read_until_eof(stdin);
-		return eval(source, "stdin");
-	}
+  // eval stdin
+  if (1 == argc) {
+    source = read_until_eof(stdin);
+    return eval(source, "stdin");
+  }
 }
